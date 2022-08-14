3e0b9d7574976f619ceca90b29d187eca75ee35e
rmrokon <mdrokon7773@gmail.com>
2022-08-15 00:49:42 +0600

    testimonials done

--- components/Testimonials.jsx
+++ components/Testimonials.jsx
@@ -1,5 +1,7 @@
 import styles from '../styles/Testimonials.module.css';
 import Circle from './Circle';
+import { reviews } from "../data";
+import Image from "next/image";
 
 export default function Testimonials() {
     return (
@@ -9,7 +11,25 @@ export default function Testimonials() {
             <h1 className={styles.title}>Testimonials</h1>
 
             <div className={styles.wrapper}>
+                {
+                    reviews.map(review => (<div
+                        className={styles.card} key={review.id}>
+                        <p className={styles.review}>
+                            {review.review}
+                        </p>
+                        <div className={styles.person}>
+                            <Image
+                                className={styles.avatar}
+                                alt=''
+                                src={review.picture}
+                                width={"30"}
+                                height={"30"}
+                            />
+                            <span className={styles.reviewerName}>{review.name}</span>
+                        </div>
 
+                    </div>))
+                }
             </div>
         </div>
     )
